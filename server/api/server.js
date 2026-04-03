3b3956b704bd0f8904befaaba3a8abc1aa912a1b
nikhilSharma1994 <me.nikhil.sharma94@gmail.com>
2026-04-03 12:34:24 +0530

    add api folder

--- server/api/server.js
+++ server/api/server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
 import http from 'http';
-import { connectDB } from './lib/db.js';
-import userRouter from './routes/userRoutes.js';
-import messageRouter from './routes/messageRoutes.js';
+import { connectDB } from '../lib/db.js';
+import userRouter from '../routes/userRoutes.js';
+import messageRouter from '../routes/messageRoutes.js';
 import { Server } from 'socket.io';
 
 
